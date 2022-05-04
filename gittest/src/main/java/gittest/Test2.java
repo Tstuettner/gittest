bef5159ca66341132473ef66f8d82f5c35c52c21
Tstuettner <66605195+Tstuettner@users.noreply.github.com>
2022-05-04 17:16:47 +0200

    Hotfix in Master.

--- gittest/src/main/java/gittest/Test2.java
+++ gittest/src/main/java/gittest/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("Test 2");
-		System.out.println("Test 2,2");
+		System.out.println("Test 2,3 Hotfix in Master.");
 
 	}
 
